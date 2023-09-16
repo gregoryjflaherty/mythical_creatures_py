a69e06d5b1a46ee7ffdaf00640305975ce5de668
Greg Flaherty <gregoryjflaherty@gmail.com>
2023-09-16 07:47:38 -0400

    add exclamation point to say function

--- tests/test_unicorn.py
+++ tests/test_unicorn.py
@@ -2,24 +2,20 @@ import pytest
 from lib.unicorn import Unicorn
 
 def test_name():
-    pytest.skip("Test is not implemented yet")
     unicorn = Unicorn('Robert')
     assert unicorn.name == 'Robert'
 
 def test_is_default_silver():
-    pytest.skip("Test is not implemented yet")
     unicorn = Unicorn('Robert')
     assert unicorn.color == 'silver'
     assert unicorn.is_silver() == True
 
 def test_doesnt_have_to_be_silver():
-    pytest.skip("Test is not implemented yet")
     unicorn = Unicorn('Barbara', 'purple')
     assert unicorn.color == 'purple'
     assert unicorn.is_silver() == False
 
 def test_says_sparkly_stuff():
-    pytest.skip("Test is not implemented yet")
     unicorn = Unicorn('Johnny')
-    assert unicorn.say('Wonderful!') == '**;* Wonderful **;*'
+    assert unicorn.say('Wonderful!') == '**;* Wonderful! **;*'
     assert unicorn.say('I dont like you very much.') == '**;* I dont like you very much. **;*'
